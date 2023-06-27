63985837b47a5f54df537aeeab05c82a810b52d6
junior <antonioivo.3@gmail.com>
2023-06-27 15:51:55 -0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
@@ -16,12 +15,16 @@ export default defineComponent({
 </script>
 
 <style>
+*{
+  background-color: rgb(1, 1, 1);
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
 }
 </style>
